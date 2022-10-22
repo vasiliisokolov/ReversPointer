611f861ee53d8fb50992bb6715050ae1b713f0fb
VasiliiSokolov <vasilysokolov@mail.ru>
2022-10-22 19:03:57 +0300

    Добавьте файлы проекта.

---
+++ ReversPointer.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+
+
+void reverse_massive(int *arr)
+{
+    
+    int temp;
+    int j = 9;
+    for (int i = 0; i < 5; i++)
+    {
+        int temp = *(arr+i);
+        *(arr+i) = *(arr+j);
+        *(arr+j) = temp;
+        j--;
+       
+    }
+}
+
+int main()
+{
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    
+    std::cout << "The array before revers:\n";
+
+      for(int i = 0; i < 10; i++)
+    {
+        std::cout << *(arr+i) << " ";
+    }
+    std::cout << std::endl;
+    
+    reverse_massive(arr);
+    
+    std::cout << "The array after revers:\n";
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << *(arr + i) << " ";
+
+    }
+}
